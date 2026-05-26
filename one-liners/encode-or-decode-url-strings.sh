b3d19b4c0fff3fb21c0af14c6e0e9004cd620cfa
zoned out <126989297+phntmzn@users.noreply.github.com>
2026-05-26 10:20:01 -0500

    Update encode-or-decode-url-strings.sh

--- one-liners/encode-or-decode-url-strings.sh
+++ one-liners/encode-or-decode-url-strings.sh
@@ -1,1 +1,5 @@
+# encode a string
 perl -MURI::Escape -e 'print uri_escape($ARGV[0])' 'string to encode'
+
+# decode a string
+perl -MURI::Escape -e 'print uri_unescape($ARGV[0])' 'encoded%20string'
